d934ae296c8e3d692e6f99f0bac6b116aa48aff0
Behram <behram89@gmail.com>
2020-09-10 00:36:58 +0200

    Fortsätt påjobbet

--- src/com/TallaVa/Main.java
+++ src/com/TallaVa/Main.java
@@ -7,10 +7,6 @@ public class Main {
     public static void main(String[] args) {
 // Variablerna
 
-        int tid1;       //Första löparens tid.
-        int tid2;       //Andra löparens tid.
-        int tid3;       //Tredje löparens tid.
-
         String name1;   //Första löparens namn.
         String name2;   //Andra löparens namn.
         String name3;   //Tredje löparens namn.
@@ -19,19 +15,23 @@ public class Main {
         Scanner keyboard = new Scanner(System.in);
 
         //Inläsning av första löparens namn.
-        System.out.println("Skriv in första löparens namn: ");
+        System.out.println("Skriv in första löparens namn och Tid: ");
         name1 = keyboard.nextLine();
 
-        //Inläsning av andra löparens namn.
-        System.out.println("Skriv in den andre löparens namn: ");
-        name2 = keyboard.nextLine();
 
-        //Inläsning av tredje löparens namn.
-        System.out.println("Skriv in den tredje löparens namn: ");
-        name3 = keyboard.nextLine();
+            //Inläsning av andra löparens namn.
+            System.out.println("Skriv in den andre löparens namn: ");
+            name2 = keyboard.nextLine();
+
+                //Inläsning av tredje löparens namn.
+                System.out.println("Skriv in den tredje löparens namn: ");
+                name3 = keyboard.nextLine();
 
         //Test
-        System.out.println(name1 + " " + name2 + " " + name3);
+
+
+        if (name1 < name2)
+            System.out.println(name1);
 
         System.exit(0);
     }
