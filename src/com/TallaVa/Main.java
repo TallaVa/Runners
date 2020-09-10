1897bec97a86d5d6c416f5ec341f518fb6050e8c
bere <behram89@gmail.com>
2020-09-10 16:15:45 +0200

    namn och tider efterfrågas.

--- src/com/TallaVa/Main.java
+++ src/com/TallaVa/Main.java
@@ -7,6 +7,10 @@ public class Main {
     public static void main(String[] args) {
 // Variablerna
 
+        int tid1;
+        int tid2;
+        int tid3;
+
         String name1;   //Första löparens namn.
         String name2;   //Andra löparens namn.
         String name3;   //Tredje löparens namn.
@@ -15,23 +19,36 @@ public class Main {
         Scanner keyboard = new Scanner(System.in);
 
         //Inläsning av första löparens namn.
-        System.out.println("Skriv in första löparens namn och Tid: ");
+        System.out.println("Skriv in första löparens namn: ");
         name1 = keyboard.nextLine();
+        System.out.println("Vad hade han för tid ? ");
+        tid1 = keyboard.nextInt();
+
+        //Rensa nexLine
+        keyboard.nextLine();
 
 
-            //Inläsning av andra löparens namn.
-            System.out.println("Skriv in den andre löparens namn: ");
-            name2 = keyboard.nextLine();
+        //Inläsning av andra löparens namn.
+        System.out.println("Skriv in den andre löparens namn: ");
+        name2 = keyboard.nextLine();
+        System.out.println("Vad hade han för tid ? ");
+        tid2 = keyboard.nextInt();
 
-                //Inläsning av tredje löparens namn.
-                System.out.println("Skriv in den tredje löparens namn: ");
-                name3 = keyboard.nextLine();
+        //Rensa nexLine
+        keyboard.nextLine();
+
+        //Inläsning av tredje löparens namn.
+        System.out.println("Skriv in den tredje löparens namn: ");
+        name3 = keyboard.nextLine();
+        System.out.println("Vad hade han för tid ? ");
+        tid3 = keyboard.nextInt();
+
+        //Rensa nextLine
+        keyboard.nextLine();
 
         //Test
 
 
-        if (name1 == name2)
-            System.out.println(name1);
 
         System.exit(0);
     }
